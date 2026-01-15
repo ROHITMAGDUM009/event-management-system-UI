b8b8741e46c3f10b6c063d24d15b5cbb7c0fc77b
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2026-01-15 19:55:04 +0530

    update: update OrganizerDashboard

--- src/dashboards/OrganizerDashboard.jsx
+++ src/dashboards/OrganizerDashboard.jsx
@@ -3,7 +3,7 @@ import DashboardLayout from "../layouts/DashboardLayout";
 const OrganizerDashboard = () => {
   return (
     <DashboardLayout>
-      <h1 className="text-2xl font-bold mb-4">Organizer Panel</h1>
+      <h1 className="text-2xl font-bold mb-4">OrganizerPanel</h1>
 
       <p className="text-gray-600">Create and manage your events.</p>
     </DashboardLayout>
