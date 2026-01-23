85069e08eba875ab8cd695c86aac75608c51927b
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-23 17:57:49 +0530

    update -: DashboardLayout

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,35 +1,12 @@
-import { useAuth } from "../context/AuthContext";
+import UserSidebar from "../components/UserSidebar";
 
 const DashboardLayout = ({ children }) => {
-  const { user, logout } = useAuth();
-
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      {/* SIDEBAR */}
-      <aside className="w-64 bg-gray-900 text-white p-6">
-        <h2 className="text-xl font-bold mb-6">{user.role} Dashboard</h2>
-
-        <nav className="space-y-3">
-          <p className="hover:text-blue-400 cursor-pointer">Dashboard</p>
-
-          {user.role === "ORGANIZER" && (
-            <p className="hover:text-blue-400 cursor-pointer">Manage Events</p>
-          )}
-
-          {user.role === "ADMIN" && (
-            <p className="hover:text-blue-400 cursor-pointer">Manage Users</p>
-          )}
-          <button
-            onClick={logout}
-            className="bg-red-600 text-white px-4 py-1 rounded hover:bg-red-700"
-          >
-            Logout
-          </button>
-        </nav>
-      </aside>
-
-      {/* MAIN CONTENT */}
-      <main className="flex-1 p-8">{children}</main>
+    <div className="flex">
+      <UserSidebar />
+      <main className="flex-1 p-6 bg-gray-100 min-h-screen">
+        {children}
+      </main>
     </div>
   );
 };
