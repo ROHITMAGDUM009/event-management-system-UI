265e723afcbde88ef01ba45d3a3eb9a497fe206c
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-25 00:22:38 +0530

    feat -: AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,34 +1,55 @@
-import { useAuth } from "../context/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Events from "../pages/Events";
+import EventDetails from "../pages/EventDetails";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+import MainLayout from "../layouts/MainLayout";
 import DashboardLayout from "../layouts/DashboardLayout";
+import Dashboard from "../pages/Dashboard";
+import MyBookings from "../pages/MyBookings";
+import ProtectedRoute from "./ProtectedRoute";
 
-import UserDashboard from "../dashboards/UserDashboard";
-import OrganizerDashboard from "../dashboards/OrganizerDashboard";
-import AdminDashboard from "../dashboards/AdminDashboard";
+import MyEvents from "../pages/MyEvents";
 
-const Dashboard = () => {
-  const { user } = useAuth();
-
-  if (!user) return null;
-
-  const renderDashboard = () => {
-    switch (user.role) {
-      case "ROLE_ADMIN":
-        return <AdminDashboard />;
-
-      case "ROLE_ORGANIZER":
-        return <OrganizerDashboard />;
-
-      case "ROLE_USER":
-      default:
-        return <UserDashboard />;
-    }
-  };
-
+const AppRoutes = () => {
   return (
-    <DashboardLayout>
-      {renderDashboard()}
-    </DashboardLayout>
+    <BrowserRouter>
+      <Routes>
+
+        {/* ================= PUBLIC ROUTES ================= */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/events/:id" element={<EventDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        {/* ================= DASHBOARD ROUTES ================= */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
+          {/* Default dashboard page */}
+          <Route index element={<Dashboard />} />
+
+          {/* User pages */}
+          <Route path="bookings" element={<MyBookings />} />
+
+          {/* ORGANIZER */}
+          <Route path="my-events" element={<MyEvents />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 };
 
-export default Dashboard;
+export default AppRoutes;
