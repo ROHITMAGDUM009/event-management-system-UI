8d6b1352209ac648898a2afb106abdcd4fd75b91
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-25 00:22:16 +0530

    update -: MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const MainLayout = ({ children }) => {
+const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow bg-gray-100">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
