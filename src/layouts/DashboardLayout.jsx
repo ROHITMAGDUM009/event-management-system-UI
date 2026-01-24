fa35d4112e41df9e5b3b1221a2fcc18167274a30
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-25 00:22:11 +0530

    update -: DashboardLayout

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,11 +1,30 @@
+import { Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 import UserSidebar from "../components/UserSidebar";
+import OrganizerSidebar from "../components/OrganizerSidebar";
+import AdminSidebar from "../components/AdminSidebar";
 
-const DashboardLayout = ({ children }) => {
+const DashboardLayout = () => {
+  const { user } = useAuth();
+
+  const renderSidebar = () => {
+    if (!user) return null;
+
+    switch (user.role) {
+      case "ROLE_ADMIN":
+        return <AdminSidebar />;
+      case "ROLE_ORGANIZER":
+        return <OrganizerSidebar />;
+      default:
+        return <UserSidebar />;
+    }
+  };
+
   return (
-    <div className="flex">
-      <UserSidebar />
-      <main className="flex-1 p-6 bg-gray-100 min-h-screen">
-        {children}
+    <div className="flex min-h-screen bg-gray-100">
+      {renderSidebar()}
+      <main className="flex-1 p-6">
+        <Outlet />
       </main>
     </div>
   );
