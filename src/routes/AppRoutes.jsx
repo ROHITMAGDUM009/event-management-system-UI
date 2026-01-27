6ca684262c9434d8cbb8e864c8e75b64db8b8fd8
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-27 10:36:15 +0200

    feat: AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,25 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+/* PUBLIC */
 import Home from "../pages/Home";
 import Events from "../pages/Events";
 import EventDetails from "../pages/EventDetails";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
+/* LAYOUTS */
 import MainLayout from "../layouts/MainLayout";
 import DashboardLayout from "../layouts/DashboardLayout";
-import Dashboard from "../pages/Dashboard";
-import MyBookings from "../pages/MyBookings";
+
+/* AUTH */
 import ProtectedRoute from "./ProtectedRoute";
 
-import MyEvents from "../pages/MyEvents";
+/* DASHBOARDS */
+import Dashboard from "../pages/Dashboard";
+
+/* USER PAGES */
+import MyBookings from "../pages/MyBookings";
+
+/* ORGANIZER PAGES */
+import OrganizerMyEvents from "../pages/OrganizerMyEvents";
+import OrganizerBookings from "../pages/OrganizerBookings";
+import CreateEvent from "../pages/CreateEvent";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
 
-        {/* ================= PUBLIC ROUTES ================= */}
+        {/* ================= PUBLIC ================= */}
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/events" element={<Events />} />
@@ -28,24 +39,38 @@ const AppRoutes = () => {
           <Route path="/register" element={<Register />} />
         </Route>
 
-        {/* ================= DASHBOARD ROUTES ================= */}
+        {/* ================= USER AREA ================= */}
         <Route
-          path="/dashboard"
+          path="/user"
           element={
             <ProtectedRoute>
               <DashboardLayout />
             </ProtectedRoute>
           }
         >
-          {/* Default dashboard page */}
-          <Route index element={<Dashboard />} />
-
-          {/* User pages */}
-          <Route path="bookings" element={<MyBookings />} />
-
-          {/* ORGANIZER */}
-          <Route path="my-events" element={<MyEvents />} />
+          <Route index element={<UserDashboard />} />
+          <Route path="my-bookings" element={<MyBookings />} />
+          <Route path="payments" element={<MyBookings />} />
         </Route>
+
+        {/* ================= ORGANIZER AREA ================= */}
+        <Route
+          path="/organizer"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<OrganizerDashboard />} />
+          <Route path="my-events" element={<OrganizerMyEvents />} />
+          <Route path="create-event" element={<CreateEvent />} />
+          <Route path="bookings" element={<OrganizerBookings />} />
+        </Route>
+
+
+        {/* ================= ADMIN AREA ================= */}
+
 
       </Routes>
     </BrowserRouter>
