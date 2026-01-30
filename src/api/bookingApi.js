0186c6f28f2fd3d39f874989e1fa6b94e789a427
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-30 15:52:03 +0530

    update: bookingApi

--- src/api/bookingApi.js
+++ src/api/bookingApi.js
@@ -1,10 +1,13 @@
 import api from "./axios";
 
+// Book an event
 export const bookEvent = (eventId) =>
   api.post("/api/bookings", { eventId });
 
-export const myBookings = () =>
+// Fetch logged-in user's bookings
+export const fetchMyBookings = () =>
   api.get("/api/bookings/my");
 
+// Pay for a booking
 export const payForBooking = (bookingId) =>
   api.post(`/api/payments/pay/${bookingId}`);
