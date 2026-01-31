b1b12d0e8d2605cb0ed2b1eff2311d316fbf0946
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-31 20:21:34 +0530

    feat: AdminPayments

---
+++ src/pages/admin/AdminPayments.jsx
@@ -0,0 +1,15 @@
+const AdminPayments = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-6">Payments</h1>
+
+            <div className="bg-white shadow p-6 rounded">
+                <p>Total Revenue: <b>₹2,40,000</b></p>
+                <p>Successful Payments: 320</p>
+                <p>Pending Payments: 12</p>
+            </div>
+        </div>
+    );
+};
+
+export default AdminPayments;
